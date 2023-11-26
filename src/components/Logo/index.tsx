247b9a5cfcd5db784ab112a19695bc2b145c3b47
Constantine <asbest911@gmail.com>
2023-11-26 21:04:33 +0300

    fix: fix homepage

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,5 @@
 import logoPath from '../../assets/images/icon-prosepto.png';
-import styles from './Logo.module.css';
 
 export default function Logo() {
-  return (
-    <img src={logoPath} alt="Логотип проекта" className={styles.logo}></img>
-  );
+  return <img src={logoPath} alt="Логотип проекта"></img>;
 }
