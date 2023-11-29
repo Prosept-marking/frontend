28a5b264faa13918643a4da2272bf94b74ea9263
Constantine <asbest911@gmail.com>
2023-11-29 18:41:52 +0300

    refactor: replace Main

---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,11 @@
+import FilterList from '../../components/FilterList';
+import Main from '../../components/Main';
+
+export default function MainPage() {
+  return (
+    <main className="main">
+      <FilterList />
+      <Main />
+    </main>
+  );
+}
